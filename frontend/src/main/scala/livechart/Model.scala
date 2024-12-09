8b6b5e03d1ab3f56912441396619412a0ddb7091
Ro0t-set <tommaso.patriti@studio.unibo.it>
2024-12-09 10:58:56 +0100

    feat(frontend): add sbt.json, build.sbt, index.html, javascript.svg, main.js, package.json, build.properties, plugins.sbt, vite.svg, LiveChart.scala, Model.scala, ModelTest.scala, style.css, vite.config.js for frontend project

---
+++ frontend/src/main/scala/livechart/Model.scala
@@ -0,0 +1,37 @@
+package livechart
+
+import scala.util.Random
+
+import com.raquo.laminar.api.L.{*, given}
+
+final class NodeID
+
+case class NodeItem(id: NodeID, label: String, x: Int, y: Int):
+  def position: Tuple = (x, y)
+
+object NodeItem:
+  def apply(): NodeItem =
+    NodeItem(NodeID(), "?", Random.nextInt(100) + 1, Random.nextInt(100) + 1)
+end NodeItem
+
+type DataList = List[NodeItem]
+
+final class Model:
+  private val dataVar: Var[DataList] = Var(List(NodeItem(NodeID(), "one", 1, 1)))
+  val dataSignal: StrictSignal[DataList] = dataVar.signal
+
+  def addNodeItem(item: NodeItem): Unit =
+    dataVar.update(data => data :+ item)
+
+  def removeNodeItem(id: NodeID): Unit =
+    dataVar.update(data => data.filter(_.id != id))
+
+  def makeNodeItemUpdater[A](id: NodeID,
+                             f: (NodeItem, A) => NodeItem): Observer[A] =
+    dataVar.updater { (data, newValue) =>
+      data.map { item =>
+        if item.id == id then f(item, newValue) else item
+      }
+    }
+  end makeNodeItemUpdater
+end Model
