66b74571817956c4589b0a53248e2564b4219cc2
Ro0t-set <tommaso.patriti@studio.unibo.it>
2024-12-16 19:48:23 +0100

    refactor(Simulator.scala): restructure code for better readability and maintainability, improve spacing and indentation for consistency, update method signatures for clarity, and optimize iteration execution for simulation process.

--- frontend/src/main/scala/simulator/Simulator.scala
+++ frontend/src/main/scala/simulator/Simulator.scala
@@ -1,39 +1,39 @@
 package simulator
 
-import domain.{AddNode, Edge, Id, Node, Position, SetEdgesByIds, SetNodes}
+import domain.{Id, Node, Position, SetEdgesByIds, SetNodes}
 import it.unibo.scafi.config.Grid3DSettings
-import state.GraphModel.{commandObserver, nodes}
-
-import scala.Option.option2Iterable
+import state.GraphModel.{commandObserver}
 import scala.concurrent.Future
-import scala.scalajs.js.annotation.JSExportTopLevel
-import scala.scalajs.js.timers.{setInterval, setTimeout}
+
 import scala.util.Random
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.scalajs.js
 
-
-object Simulator :
-  def startSimulation(iterations: Int, delay: Int): Unit =
-    import it.unibo.scafi.config.GridSettings
+object Simulator {
+  def startSimulation(iterations: Int, delay: Int): Unit = {
     import it.unibo.scafi.incarnations.BasicAbstractSpatialSimulationIncarnation
     import it.unibo.scafi.space.{Point3D, SpaceHelper}
 
-    object BasicSpatialIncarnation extends BasicAbstractSpatialSimulationIncarnation:
+    object BasicSpatialIncarnation extends BasicAbstractSpatialSimulationIncarnation {
       override type P = Point3D
       trait MyDistanceStrategy extends DistanceStrategy
       override def buildNewSpace[E](elems: Iterable[(E, P)]): SPACE[E] =
         new Basic3DSpace(elems.toMap) with MyDistanceStrategy
+    }
     import BasicSpatialIncarnation._
-    object DemoSpatial extends AggregateProgram with StandardSensors:
-      def main(): Int = rep(0){ _ + 1 }
+
+    object DemoSpatial extends AggregateProgram with StandardSensors {
+      def main(): Int = rep(0) { _ + 1 }
+    }
 
     val (ncols, nrows, ndeep) = (10, 10, 2)
     val (stepx, stepy, stepz) = (100, 100, 100)
-    val positions: List[Point3D] = SpaceHelper.grid3DLocations(Grid3DSettings(nrows, ncols, ndeep,  stepx, stepy, stepz, tolerance = 0))
-    val ids: IndexedSeq[Int] = for (i <- 1 to ncols * nrows * ndeep) yield i
+    val positions: List[Point3D] = SpaceHelper.grid3DLocations(
+      Grid3DSettings(nrows, ncols, ndeep, stepx, stepy, stepz, tolerance = 0)
+    )
+    val ids: IndexedSeq[Int] = (1 to ncols * nrows * ndeep).toIndexedSeq
     val devsToPos: Map[Int, Point3D] = ids.zip(positions).toMap
-
 
     val net = new SpaceAwareSimulator(
       space = new Basic3DSpace(devsToPos, proximityThreshold = 200),
@@ -45,34 +45,52 @@ object Simulator :
       randomSensorSeed = System.currentTimeMillis()
     )
 
+    def executeIterations(
+                           current: Int,
+                           iterations: Int,
+                           ids: Seq[Int],
+                           simulationRandom: scala.util.Random,
+                           net: SpaceAwareSimulator,
+                           executedNodes: Seq[Int]
+                         ): Unit = {
+      if (current >= iterations) {
+      } else {
+        val nextIdToRun = ids(simulationRandom.nextInt(ids.size))
+        val updatedExecutedNodes = executedNodes :+ nextIdToRun
 
-    val simulationRandom = new Random(1)
-    var executedNodes = Seq[ID]()
-    for (i <- 0 until iterations)
-      val nextIdToRun = ids(simulationRandom.nextInt(ids.size))
-      executedNodes :+= nextIdToRun
-      Future{
-        net.exec(DemoSpatial, DemoSpatial.main(), nextIdToRun)
-        //if i % 100 == 0 then action(i)
-        action(i)
-      }
 
-
-      def action(i: Int): Unit =
-        val nodes = net.devs.map(d => Node(
-          d._1,
-          Position(d._2.pos.x, d._2.pos.y, d._2.pos.z),
-          net.`export`(d._1).map(_.root().toString).getOrElse("."),
-          0x00ff)).toSet
-
-        val edges: Set[(Id, Id)] = net.devs
-          .flatMap { d =>
-            net.neighbourhood(d._1).map(nbr => (d._1, nbr))
+        Future {
+          net.exec(DemoSpatial, DemoSpatial.main(), nextIdToRun)
+          action(current)
+        }.onComplete { _ =>
+          js.Dynamic.global.requestAnimationFrame { (_: Double) =>
+            executeIterations(current + 1, iterations, ids, simulationRandom, net, updatedExecutedNodes)
           }
-          .filter { case (a, b) => a < b }
-          .toSet
-        println(edges.size)
-        commandObserver.onNext(SetNodes(nodes))
-        commandObserver.onNext(SetEdgesByIds(edges))
+        }
+      }
+    }
 
+    def action(i: Int): Unit = {
+      val nodes = net.devs.map { case (id, devInfo) =>
+        Node(
+          id,
+          Position(devInfo.pos.x, devInfo.pos.y, devInfo.pos.z),
+          net.`export`(id).map(_.root().toString).getOrElse("."),
+          0x00ff
+        )
+      }.toSet
 
+      val edges: Set[(Id, Id)] = net.devs
+        .flatMap { case (d, _) =>
+          net.neighbourhood(d).map(nbr => (d, nbr))
+        }
+        .filter { case (a, b) => a < b }
+        .toSet
+
+      commandObserver.onNext(SetNodes(nodes))
+      commandObserver.onNext(SetEdgesByIds(edges))
+    }
+
+    executeIterations(0, iterations, ids, new Random(), net, Seq.empty)
+  }
+}
