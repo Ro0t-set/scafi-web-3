37eb348fa6287cd49afd1a7b2867c36b6984a401
Ro0t-set <tommaso.patriti@studio.unibo.it>
2025-01-06 20:06:58 +0100

    refactor(EngineController.scala): remove unnecessary println statement in handleNewData method

--- src/main/scala/view/player/EngineController.scala
+++ src/main/scala/view/player/EngineController.scala
@@ -16,7 +16,6 @@ object EngineController:
     engine.now().nextAndGetJsonNetwork()
   private def handleNewData(nodes: js.Dynamic): Unit =
     val nodesJson = JSON.stringify(nodes)
-    println(s" = ${nodesJson}")
     GraphAPI.addNodesFromJson(nodesJson)
   private def start(): Unit =
     def loop(): Unit =
