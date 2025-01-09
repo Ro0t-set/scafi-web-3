f54e95d5ed5f492d8371ee617eb461ed17845d02
Ro0t-set <tommaso.patriti@studio.unibo.it>
2025-01-09 14:43:30 +0100

    refactor(EngineController.scala): refactor EngineController to use more descriptive variable names and improve code readability feat(EngineController.scala): add trait EngineControllerImpl with methods start(), processNextBatch(), and handleNewData() for better code organization and modularity

--- src/main/scala/view/player/EngineController.scala
+++ src/main/scala/view/player/EngineController.scala
@@ -3,37 +3,41 @@ package view.player
 import API.GraphAPI
 import domain.NextTick
 import com.raquo.laminar.api.L.{*, given}
-
 import scala.scalajs.js
 import state.AnimationState.{animationObserver, batch, engine, running}
-
 import scala.scalajs.js.JSON
 import scala.scalajs.js.timers.setTimeout
 
-object EngineController:
-  private type n           = js.Dynamic
-  private type e           = js.Dynamic
-  private type JsonNetwork = (n, e)
+trait EngineControllerImpl:
+  type N = js.Dynamic
+  type E = js.Dynamic
+  type JsonNetwork = (N, E)
 
+  def start(): Unit
+  def processNextBatch(): JsonNetwork
+  def handleNewData(net: JsonNetwork): Unit
+
+object EngineControllerImpl extends EngineControllerImpl:
   running.signal.foreach {
     case true  => start()
     case false => ()
   }(unsafeWindowOwner)
-  private def processNextBatch(): JsonNetwork =
+
+  def processNextBatch(): JsonNetwork =
     engine.now().getOrElse(js.Dynamic.literal()).executeIterations()
-    val net: n  = engine.now().getOrElse(js.Dynamic.literal()).getNodes()
-    val edge: e = engine.now().getOrElse(js.Dynamic.literal()).getEdges()
+    val net: N  = engine.now().getOrElse(js.Dynamic.literal()).getNodes()
+    val edge: E = engine.now().getOrElse(js.Dynamic.literal()).getEdges()
     (net, edge)
 
-  private def handleNewData(net: JsonNetwork): Unit =
+  def handleNewData(net: JsonNetwork): Unit =
     net match
       case (nodes: js.Dynamic, edges: js.Dynamic) =>
-        val nodesJson: String = JSON.stringify(nodes)
-        val edgesJson: String = JSON.stringify(edges)
+        val nodesJson = JSON.stringify(nodes)
+        val edgesJson = JSON.stringify(edges)
         GraphAPI.addNodesFromJson(nodesJson)
         GraphAPI.addEdgesFromJson(edgesJson)
 
-  private def start(): Unit =
+  def start(): Unit =
     def loop(): Unit =
       if running.signal.now() then
         val batchValue = batch.signal.now()
