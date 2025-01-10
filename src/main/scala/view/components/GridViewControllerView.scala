df740efcae5a3713f9588c7ef45ace635b934908
Ro0t-set <tommaso.patriti@studio.unibo.it>
2025-01-10 11:26:44 +0100

    feat(GridViewControllerView.scala): add 2D Mode button to switch scene to 2D mode for better user experience

--- src/main/scala/view/components/GridViewControllerView.scala
+++ src/main/scala/view/components/GridViewControllerView.scala
@@ -6,5 +6,6 @@ import view.graph.scene.GraphThreeScene
 class GridViewControllerView(scene: GraphThreeScene) extends ViewComponent:
   override def render: Element =
     div(
-      button("Center View", onClick --> (_ => scene.centerView()))
+      button("Center View", onClick --> (_ => scene.centerView())),
+      button("2D Mode", onClick --> (_ => scene.set2DMode()))
     )
